dc4ea340369672da9c022ebe57b99028d8e59458
Xu Ren <musicalxu@hotmail.com>
2022-03-16 08:43:44 +0800

    start responsive front-end

--- src/public/client.js
+++ src/public/client.js
@@ -1,46 +1,64 @@
-let store = {
-    user: { name: "Student" },
+let store = Immutable.Map({
+    user: Immutable.Map({
+        name: "Student"
+    }),
     apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+    rovers: Immutable.List(['Curiosity', 'Opportunity', 'Spirit']),
+    view: 'Landing'
+})
 
 // add our markup to the page
 const root = document.getElementById('root')
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
+const updateStore = (state, newState) => {
+    store = state.merge(newState);
+    // console.log(store.toJS());
+
+    render(root, store);
 }
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
+    console.log("Called render:")
+    root.innerHTML = App(state);
 }
 
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
 
-    return `
-        <header></header>
-        <main>
-            ${Greeting(store.user.name)}
-            <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
+    let rovers = state.get("rovers");
+    let apod = state.get("apod");
+
+    // console.log(apod)
+
+
+    switch (state.get("view")) {
+        case "Landing":
+            return viewLandingPage();
+        default:
+            return `Hello`
+    }
+
+    // return `
+    //     <header> </header>
+    //     <main>
+    //         ${Greeting(state.getIn(["user", "name"]))}
+    //         <section>
+    //             <h3>Put things on the page!</h3>
+    //             <p>Here is an example section.</p>
+    //             <p>
+    //                 One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
+    //                 the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
+    //                 This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
+    //                 applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
+    //                 explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
+    //                 but generally help with discoverability of relevant imagery.
+    //             </p>
+    //             ${ImageOfTheDay(apod)}
+    //         </section>
+    //     </main>
+    //     <footer>Xu Ren for Udacity JavaScript</footer>
+    // `
 }
 
 // listening for load event because page should load before any JS is called
@@ -63,16 +81,37 @@ const Greeting = (name) => {
     `
 }
 
-// Example of a pure function that renders infomation requested from the backend
+const viewLandingPage = () => {
+    return `
+        <div>
+            <h3>Welcome to Space Exploration with the NASA API</h3>
+        </div>
+
+        <div>
+            <p>Explore Mars Rover Images</p>
+            <div class='row'>
+                <div class='col-1'>Curiosity</div>
+                <div class='col-1'>Opportunity</div>
+                <div class='col-1'>Spirit</div>
+            </div>
+        </div>
+
+        <div>
+            <p>Learn about the astronomical photo of the day</p>
+            <button>Show Image</button>
+        </div>
+    `
+}
+
+const viewRover = (rover) => {
+    return `Hello ${rover}`
+}
+
+
+// Example of a pure function that renders information requested from the backend
 const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
+    ''
+    if (!store.get("apod")) {
         getImageOfTheDay(store)
     }
 
@@ -95,11 +134,17 @@ const ImageOfTheDay = (apod) => {
 
 // Example API call
 const getImageOfTheDay = (state) => {
-    let { apod } = state
+
 
     fetch(`http://localhost:3000/apod`)
         .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+        .then(
+            apod => {
+                console.log(apod);
 
-    return data
+                const newState = state.set("apod", apod);
+                updateStore(state, newState);
+
+                return apod
+            })
 }
